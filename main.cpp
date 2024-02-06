efe5c8b24154dafcafc92f1a75cdc6ba586286f3
Ulyana V. Shvetsova <uvs3@tpu.ru>
2024-02-06 13:56:34 +0700

    Xoxo

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from laba1!\n";
+    int first = 0;
+    int second = 123;
+    first = second + second;
+    std::cout << first;
+    return 0;
+}
